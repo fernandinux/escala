348646e737b9b619c81f665d176a6420f18ba99e
fernandinux <fernando.paca@gmail.com>
2020-11-24 00:24:53 -0500

    contenido2

--- index.php
+++ index.php
@@ -94,5 +94,180 @@
 
     </section>
     <!--Section: Content-->
+    <!--Section: Feature-->
+     <section class="p-md-3 mx-md-5 text-lg-left">
+    <h2 class="font-weight-bold text-center mb-5 pb-3">Features</h2>
+    <div class="row text-center d-flex justify-content-center">
+      
+      <div class="col-lg-4 col-md-6 mb-lg-0 mb-5">
+        <i class="fas fa-map-marker-alt fa-3x indigo-text mb-4"></i>
+        <h4 class="font-weight-bold mb-4">Feature 1</h4>
+        <p class="text-muted px-2 mb-lg-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-lg-0 mb-5">
+        <i class="far fa-map pink-text fa-3x mb-4"></i>
+        <h4 class="font-weight-bold mb-4">Feature 2</h4>
+        <p class="text-muted px-2 mb-lg-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+      <div class="col-lg-4 col-md-6 mb-md-0 mb-5">
+        <i class="fas fa-globe-africa fa-3x amber-text mb-4"></i>
+        <h4 class="font-weight-bold mb-4">Feature 3</h4>
+        <p class="text-muted px-2 mb-md-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+     
+    </div>
+  </section>
+  <!--Section: Call to action-->
+    <section class="px-md-5 mx-md-5 text-center dark-grey-text">
+
+      <!--Grid row-->
+      <div class="row d-flex justify-content-center">
+
+        <!--Grid column-->
+        <div class="col-xl-6 col-md-8">
+
+          <h3 class="font-weight-bold">Call to action</h3>
+
+          <p class="text-muted">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente
+            molestiae
+            numquam quas, voluptates omnis nulla ea odio.</p>
+
+          <a class="btn btn-info btn-md ml-0 mb-5" href="#" role="button">Start now<i class="fa fa-magic ml-2"></i></a>
+
+          <p class="mb-4 font-weight-bold">Trusted by 1 000 000 + developers &amp; designers</p>
+
+          <!--Grid row-->
+          <div class="row">
+
+            <!--First column-->
+            <div class="col-lg-3 col-md-6 d-flex align-items-center justify-content-center">
+              <img src="https://mdbootstrap.com/img/logo/brands/nike.png" class="img-fluid" alt="Nike - logo"
+                style="max-height: 40px">
+            </div>
+            <!--/First column-->
+
+            <!--Second column-->
+            <div class="col-lg-3 col-md-6 d-flex align-items-center justify-content-center">
+              <img src="https://mdbootstrap.com/img/logo/brands/amazon.png" class="img-fluid" alt="Amazon - logo"
+                style="max-height: 40px">
+            </div>
+            <!--/Second column-->
+
+            <!--Third column-->
+            <div class="col-lg-3 col-md-6 d-flex align-items-center justify-content-center">
+              <img src="https://mdbootstrap.com/img/logo/brands/sony.png" class="img-fluid" alt="Sony - logo"
+                style="max-height: 40px">
+            </div>
+            <!--/Third column-->
+
+            <!--Fourth column-->
+            <div class="col-lg-3 col-md-6 d-flex align-items-center justify-content-center">
+              <img src="https://mdbootstrap.com/img/logo/brands/samsung.png" class="img-fluid" alt="Samsung - logo"
+                style="max-height: 40px">
+            </div>
+            <!--/Fourth column-->
+
+          </div>
+          <!--/Grid row-->
+
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+
+
+    </section>
+    <!--Section: Call to action-->
+    <!--Section: Content2-->
+    <section class="px-md-5 mx-md-5 dark-grey-text text-center">
+
+      <!--Grid row-->
+      <div class="row d-flex justify-content-center">
+
+        <!--Grid column-->
+        <div class="col-lg-8">
+
+          <!--Grid row-->
+          <div class="row">
+
+            <!--First column-->
+            <div class="col-md-3 col-6 mb-4">
+              <i class="fas fa-gem fa-3x blue-text"></i>
+            </div>
+            <!--/First column-->
+
+            <!--Second column-->
+            <div class="col-md-3 col-6 mb-4">
+              <i class="fas fa-chart-area fa-3x teal-text"></i>
+            </div>
+            <!--/Second column-->
+
+            <!--Third column-->
+            <div class="col-md-3 col-6 mb-4">
+              <i class="fas fa-cogs fa-3x indigo-text"></i>
+            </div>
+            <!--/Third column-->
+
+            <!--Fourth column-->
+            <div class="col-md-3 col-6 mb-4">
+              <i class="fas fa-cloud-upload-alt fa-3x deep-purple-text"></i>
+            </div>
+            <!--/Fourth column-->
+
+          </div>
+          <!--/Grid row-->
+
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem minima cum sapiente alias iste expedita saepe explicabo illum ab, reiciendis aliquid nulla temporibus mollitia quae beatae harum sequi quidem ad.</p>
+
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+
+
+    </section>
+    <!--Section: Content2-->
+
+    <!--Section: Content3-->
+    <section class="p-md-3 mx-md-5 text-lg-left">
+        <div class="row d-flex justify-content-center">
+        <h2 class="font-weight-bold pb-4">Features</h2>
+        </div>
+        <div class="row text-center d-flex justify-content-center mt-4">
+        <div class="col-lg-4 col-md-6 mb-5">
+            <i class="far fa-chart-bar indigo-text fa-3x mb-4"></i>
+            <h4 class="font-weight-bold mb-4">Feature 1</h4>
+            <p class="text-muted px-2 mb-lg-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5">
+            <i class="fa fa-home pink-text fa-3x mb-4"></i>
+            <h4 class="font-weight-bold mb-4">Feature 2</h4>
+            <p class="text-muted px-2 mb-lg-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+        <div class="col-lg-4 col-md-6 mb-5">
+            <i class="fa fa-plane blue-text fa-3x mb-4"></i>
+            <h4 class="font-weight-bold mb-4">Feature 3</h4>
+            <p class="text-muted px-2 mb-md-0">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+        </div>
+        </div>
+    </section>
+    <!--Section: Content3-->
 
 <?php get_footer(); ?>
