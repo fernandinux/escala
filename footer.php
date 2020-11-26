63d3f51319ed7b21a511e7cd413a68d60ce038c3
fernandinux <fernando.paca@gmail.com>
2020-11-25 23:06:15 -0500

    correcion de div

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
 <?php wp_footer(); ?>
+  <!--Section: Footer-->
+    <section class="px-md-5 mx-md-5 py-5 text-center white-text elegant-color z-depth-1 rounded">
+
+      <h3 class="">Made with <i class="fas fa-heart red-text mx-1"></i> by MDB</h3>
+
+    </section>
+    <!--Section: Footer-->
 </body>
 </html>
